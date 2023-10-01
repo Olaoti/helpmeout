2a6a573e76f5f06462bc059082f3f490aa0f154a
Olaoti <omotolaolaoti@gmail.com>
2023-10-01 23:12:27 +0100

    link to drive added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,6 +6,7 @@ import { ReactComponent as Headerbggrey } from '../Assets/bgs/headbg2.svg'
 import woman from '../Assets/images/woman.png'
 import oldwoman from '../Assets/images/oldwoman.png'
 import males from '../Assets/images/males.png'
+import { Link } from 'react-router-dom'
 
 
 function Header() {
@@ -17,7 +18,9 @@ function Header() {
         <div className='header__section__left'>
             <h2>Show Them <br/>Don't Just Tell</h2>
             <h5>Help your friends and loved ones by creating and sending videos on how to get things done on a website.</h5>
-            <button id='install' className='flex fwork'><span>Install HelpMeOut</span><Arrowright/></button>
+            <Link className='link' to={'https://drive.google.com/drive/folders/1WX_YoUqPfZVHajIHZXFKXOZd-BmVkx7C?usp=drive_link'} target='_blank'>
+            <button id='install' className='flex fwork' ><span>Install HelpMeOut</span><Arrowright/></button></Link>
+           
         </div>
         <div className='header__section__right'>
                 <div className='blue'><Headerbg/></div>
